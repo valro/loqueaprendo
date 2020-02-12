8d594c64227dc96d472b3f40e00ad1f642d75fd3
valgalsan <valgalsan@gmail.com>
2020-02-12 14:31:27 +0100

    Terminado pp_4_05.c. Desde surface

--- learn-c/src/pp_4_05.c
+++ learn-c/src/pp_4_05.c
@@ -11,4 +11,54 @@
  *
  */
  
+#include<stdio.h>
 
+int main(void)
+{
+  int number, quotient, i1, i2, i3,
+    i4, i5, i6, i7, i8, i9, i10, i11,
+    first_sum, second_sum, total, check;
+
+  printf("Enter first 11 digits of a UPC: ");
+  scanf("%11d", &number);
+
+  i11 = number % 10;
+  quotient = number / 10;
+  i10 = quotient % 10;
+  quotient = quotient / 10;
+  i9 = quotient % 10;
+  quotient = quotient / 10;
+  i8 = quotient % 10;
+  quotient = quotient / 10;
+  i7 = quotient % 10;
+  quotient = quotient / 10;
+  i6 = quotient % 10;
+  quotient = quotient / 10;
+  i5 = quotient % 10;
+  quotient = quotient / 10;
+  i4 = quotient % 10;
+  quotient = quotient / 10;
+  i3 = quotient % 10;
+  quotient = quotient / 10;
+  i2 = quotient % 10;
+  quotient = quotient / 10;
+  i1 = quotient % 10;
+
+  first_sum = i1 + i3 + i5 + i7 + i9 + i11;
+  second_sum = i2 + i4 + i6 + i8 + i10;
+  total = 3 * first_sum + second_sum;
+
+  check = (9 - (total - 1) % 10);
+
+  printf("%d %d %d %d %d %d\n", i1 , i3 , i5 , i7 , i9 , i11);
+  printf("%d %d %d %d %d\n", i2 , i4 , i6 , i8 , i10);
+  printf("------------------\n");
+  printf("first_sum = %d\n", first_sum);
+  printf("second_sum = %d\n", second_sum);
+  
+  printf("Check digit: %1d\n", check);
+
+  
+
+  return 0;
+}
