55f44fb72dfab550eb010dda5c12e4d15dd8cd18
valgalsan <valgalsan@gmail.com>
2020-02-27 12:23:45 +0100

    Sin terminar pp_6_10.c Surface

--- learn-c/src/pp_6_10.c
+++ learn-c/src/pp_6_10.c
@@ -18,10 +18,45 @@
 #include<stdio.h>
 
 int main(void) {
+  int first_day, first_month, first_year;
+  int second_day, second_month, second_year;
+  int day, month, year;
+  int min_day, min_mont, min_year;
+
+  // Introducir dos fechas y extraer la mas temprana
+  
+  printf("Enter a date (mm/dd/yy) : ");
+  scanf("%2d/%2d/%2d", &first_month, &first_day, &first_year);
+  printf("Enter a date (mm/dd/yy) : ");
+  scanf("%2d/%2d/%2d", &second_month, &second_day, &second_year);
 
   
+  // If anidados
+  
+  if (first_year < second_year)
+    min_month = first_month; min_day = first_day; min_year = first_year;
 
+  else if (first_year == second_year)
 
+    if (first_month < second_month)
+      min_month = first_month; min_day = first_day; min_year = first_year;
+
+    else if (first_month == second_month)
+
+      if (first_day < second_day)
+	min_month = first_month; min_day = first_day; min_year = first_year;
+
+      else
+	min_month = second_month; min_day = second_day; min_year = second_year;
+  
+      else
+	min_month = second_month; min_day = second_day; min_year = second_year;
+    
+  for (;;) {
+    
+
+  }
+  
   return 0;
 }
 
