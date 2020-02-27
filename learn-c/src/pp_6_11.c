f7503c79b99f8933926d4f1bcee7d8254010088e
valgalsan <valgalsan@gmail.com>
2020-02-27 20:35:06 +0100

    pp_6_11.c Surface

---
+++ learn-c/src/pp_6_11.c
@@ -0,0 +1,40 @@
+/*pp_6_11.c
+ *
+ *The value of the mathematical constant e can be expressed as an 
+ *infinte series:
+ *e=1+1/1!+1/2!+1/3!+....=1/n!
+ *
+ *Write a program that aproximates e by computing the value of
+ *e=1+1/1!+1/2!+1/3!+....=1/n!
+ *where n is an integer entered by the user.
+ *date:27/02/2020
+ *author:vgs
+ *
+ */
+
+#include<stdio.h>
+
+int main (void)  {
+  
+  float e, term;
+  int t, n;
+
+  printf("Enter the value of n: ");
+  scanf("%d", &n);
+  
+  if ( n == 0 )
+    return 1;
+
+  e = 1;
+  term = 1;
+
+  for ( t = 1; t <= n; t++ ) {
+
+    term /= t;
+    e += term;
+    
+  }
+  printf("El valor de e para %d terminos es: %.15f\n", n, e);
+  
+  return 0;
+}
